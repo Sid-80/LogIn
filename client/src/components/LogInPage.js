3ee25712ef1a3d0e796fe417a6f98b5125b3f40d
Sid-80 <101277141+Sid-80@users.noreply.github.com>
2022-11-30 00:16:19 +0530

    react setup

---
+++ client/src/components/LogInPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './LogIn.css';
+
+const LogIn = () =>{
+    return(
+        <>
+        <h1 className='Sid'>Sid</h1>
+        </>
+    )
+}
+
+export default LogIn;
