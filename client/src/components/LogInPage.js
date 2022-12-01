48a686b0a5127e3ef8014b44e8495415c41af7ac
Sid-80 <101277141+Sid-80@users.noreply.github.com>
2022-12-01 22:09:30 +0530

    clientDone

--- client/src/components/LogInPage.js
+++ client/src/components/LogInPage.js
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './LogIn.css';
 
 const LogIn = () =>{
+
+    const [userData,setUserData] = useState({
+        email : "",
+        password : ""
+    });
+    let name,value;
+    const handleInputs = (e) => {
+        console.log(e);
+        name = e.target.name;
+        value = e.target.value;
+
+        setUserData({...userData,[name] : value});
+    }
+    console.log(userData);
     return(
-        <>
-        <h1 className='Sid'>Sid</h1>
-        </>
+        <div>
+            <div className='formDiv'>
+                <div>
+                    <h1>Sign In</h1>
+                </div>
+                <div>
+                    <form method='post' className='form1'>
+                        <div>
+                            <input type='email' name='email' value={userData.email} onChange={handleInputs} placeholder='Email'></input>
+                        </div>
+                        <div>
+                            <input type='password' name='password' value={userData.password} onChange={handleInputs} placeholder='Password'></input>
+                        </div>
+                        <div className='formBut'>
+                            <button>Submit</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
     )
 }
 
