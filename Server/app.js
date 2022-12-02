de8818342ef9b1e09b272eb00c889e476ddbb729
Sid-80 <101277141+Sid-80@users.noreply.github.com>
2022-12-02 22:10:06 +0530

    created api

--- Server/app.js
+++ Server/app.js
@@ -1,11 +1,15 @@
+const dotenv = require('dotenv');//package to hide your password 
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const DB = 'mongodb+srv://siddharth:$iddharth@cluster0.gtkkhj7.mongodb.net/DLMS?retryWrites=true&w=majority';
 
-mongoose.connect(DB).then(()=>{
-    console.log("connected !!");//it works as promises if it works then it reurns connected.
-}).catch((err) => { console.log("Not connected !!") })
+dotenv.config({path:'./config.env'});
+require('./db/conn');
+const User = require('./model/userSchema');
+
+app.use(express.json()); //it will convert all json files
+app.use(require('./router/rout'));
+const PORT = process.env.PORT; //used to extract or config(hidden data)
 
 //middleware = it checks whether the user has logged in correctly or not
 const middleWare = (res,req,next) => {
@@ -13,13 +17,11 @@ const middleWare = (res,req,next) => {
     next(); //redirect to next page
 }
 
-app.get('/',(req,res) => {
-    res.send("Hello !!");
-})
+
 app.get('/home',middleWare,(req,res) => {
     res.send("Lets show the content !!");
 })
 
-app.listen(3000,()=>{
+app.listen(PORT,()=>{
     console.log("On, 3000 !!");
 })
